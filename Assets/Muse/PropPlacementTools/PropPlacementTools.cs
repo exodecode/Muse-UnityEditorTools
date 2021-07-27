45c1aba12bd236979760e2b7666a1133f0054c46
exodecode <nicholas.lee.work@gmail.com>
2021-07-27 13:20:16 -0600

    Prop Placement Tools

---
+++ Assets/Muse/PropPlacementTools/PropPlacementTools.cs
@@ -0,0 +1,14 @@
+using UnityEditor;
+using UnityEngine;
+
+public static class PropPlacementTools
+{
+    [MenuItem("Tools/PropPlacement/Set Random Y Rotation")]
+    public static void SetRandomYRotation()
+    {
+        var gameObjects = Selection.gameObjects;
+        for (int i = 0; i < gameObjects.Length; i++)
+            gameObjects[i].transform.rotation =
+                Quaternion.Euler(0, Random.Range(0f, 360f), 0);
+    }
+}
