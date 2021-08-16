450c3e1dd05152a0563969590c5c903cf63af4dd
exodecode <nicholas.lee.work@gmail.com>
2021-08-16 15:01:20 -0600

    Imporoved sort by texture and added uv texture

--- Assets/Muse/EditorTools/GameObjectTools.cs
+++ Assets/Muse/EditorTools/GameObjectTools.cs
@@ -22,17 +22,16 @@ namespace Muse
 
         public static void SortGameObjectsBasedOnHavingTexture(Transform[] transforms)
         {
-            var hasTextureParent = new GameObject("Has Texture").transform;
-            var noTextureParent = new GameObject("Needs Texture").transform;
+            var meshRenderersAndTransform = transforms.Select(t => (meshRenderers: t.GetComponentsInChildren<MeshRenderer>(), transform: t));
 
-            for (int i = 0; i < transforms.Length; i++)
-            {
-                var t = transforms[i];
-                var renderers = t.GetComponentsInChildren<MeshRenderer>();
-                var hasTexture = renderers.All(r => r.sharedMaterial.GetTexture("_MainTex") != null);
+            var sorted =
+                meshRenderersAndTransform
+                .OrderByDescending(pair => pair.meshRenderers.All(mr => mr.sharedMaterial.GetTexture("_MainTex") != null))
+                .Select(p => p.transform)
+                .ToArray();
 
-                t.SetParent(hasTexture ? hasTextureParent : noTextureParent);
-            }
+            for (int i = 0; i < sorted.Length; i++)
+                sorted[i].SetSiblingIndex(i);
         }
 
         public static void SortGameObjectsAlpabetically(Transform[] transforms)
@@ -43,73 +42,5 @@ namespace Muse
             for (int i = 0; i < length; i++)
                 sorted[i].SetSiblingIndex(i);
         }
-
-        // public static void AddMeshCollidersBasedOnChildMeshes(Transform[] transforms)
-        // {
-        //     for (int i = 0; i < transforms.Length; i++)
-        //     {
-        //         var t = transforms[i];
-        //         var meshFilters = t.GetComponentsInChildren<MeshFilter>();
-
-        //         for (int j = 0; j < meshFilters.Length; j++)
-        //         {
-        //             var mc = t.gameObject.AddComponent<MeshCollider>();
-        //             mc.sharedMesh = meshFilters[j].sharedMesh;
-        //         }
-        //     }
-        // }
-
-        // public static void RemoveAllColliders(Transform[] transforms)
-        // {
-        //     // var go = new GameObject("Helper");
-        //     // var helper = go.AddComponent<GameObjectToolHelper>();
-
-        //     for (int i = 0; i < transforms.Length; i++)
-        //     {
-        //         var t = transforms[i];
-        //         var colliders = t.GetComponentsInChildren<Collider>();
-        //         for (int j = 0; j < colliders.Length; j++)
-        //         {
-        //             var collider = colliders[j];
-        //             // helper.DestroyImmediateCollider(collider);
-        //         }
-        //     }
-
-        //     // helper.Finish();
-        // }
-
-        // public static void AddBoxColliderBasedOnChildMeshes(Transform[] transforms)
-        // {
-        //     for (int i = 0; i < transforms.Length; i++)
-        //     {
-        //         var hasBounds = false;
-        //         var t = transforms[i];
-        //         var renderers = t.GetComponentsInChildren<MeshRenderer>();
-
-        //         var bc = t.GetComponent<BoxCollider>();
-        //         if (bc == null)
-        //             bc = t.gameObject.AddComponent<BoxCollider>();
-
-        //         Bounds bounds = new Bounds(Vector3.zero, Vector3.zero);
-
-        //         for (int j = 0; j < renderers.Length; j++)
-        //         {
-        //             var renderer = renderers[j];
-        //             if (renderer != null)
-        //             {
-        //                 if (hasBounds)
-        //                     bounds.Encapsulate(renderer.bounds);
-        //                 else
-        //                 {
-        //                     bounds = renderer.bounds;
-        //                     hasBounds = true;
-        //                 }
-        //             }
-        //         }
-
-        //         bc.center = bounds.center - t.position;
-        //         bc.size = bounds.size;
-        //     }
-        // }
     }
 }
