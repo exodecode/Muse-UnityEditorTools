0a2213ab2728dbf5f6f321f765ce8c1ba0883a82
exodecode <nicholas.lee.work@gmail.com>
2021-09-03 17:11:21 -0600

    swap namespace for using in example script

--- Assets/Muse/DesignKits/Examples/ObjectPooler/ExampleObjectPooler.cs
+++ Assets/Muse/DesignKits/Examples/ObjectPooler/ExampleObjectPooler.cs
@@ -1,32 +1,29 @@
-// using Muse;
+using Muse;
 using UnityEngine;
 
-namespace Muse
+public class ExampleObjectPooler : MonoBehaviour
 {
-    public class ExampleObjectPooler : MonoBehaviour
+    public ObjectPooler objectPooler;
+
+    void Update()
     {
-        public ObjectPooler objectPooler;
+        if (Input.GetMouseButtonDown(0))
+            Spawn(0);
+        else if (Input.GetMouseButtonDown(1))
+            Spawn(1);
+        else if (Input.GetKeyDown(KeyCode.Space))
+            Spawn(2);
+        else if (Input.GetKeyDown(KeyCode.C))
+            objectPooler.CleanupAllPools();
+        else if (Input.GetKeyDown(KeyCode.A))
+            objectPooler.CleanupPool(0);
+    }
 
-        void Update()
-        {
-            if (Input.GetMouseButtonDown(0))
-                Spawn(0);
-            else if (Input.GetMouseButtonDown(1))
-                Spawn(1);
-            else if (Input.GetKeyDown(KeyCode.Space))
-                Spawn(2);
-            else if (Input.GetKeyDown(KeyCode.C))
-                objectPooler.CleanupAllPools();
-            else if (Input.GetKeyDown(KeyCode.A))
-                objectPooler.CleanupPool(0);
-        }
-
-        void Spawn(int index)
-        {
-            var g = objectPooler.DrawFromPool(index);
-            g.transform.position = Vector3.zero;
-            g.transform.rotation = Quaternion.identity;
-            g.GetComponent<Rigidbody>().velocity = Vector3.zero;
-        }
+    void Spawn(int index)
+    {
+        var g = objectPooler.DrawFromPool(index);
+        g.transform.position = Vector3.zero;
+        g.transform.rotation = Quaternion.identity;
+        g.GetComponent<Rigidbody>().velocity = Vector3.zero;
     }
 }
