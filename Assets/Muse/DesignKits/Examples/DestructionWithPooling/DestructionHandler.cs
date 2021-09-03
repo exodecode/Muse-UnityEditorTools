0604df8627c2ce93e820ad22bfa8fe4b94b99ca0
exodecode <nicholas.lee.work@gmail.com>
2021-09-03 17:09:45 -0600

    namespaces

--- Assets/Muse/DesignKits/Examples/DestructionWithPooling/DestructionHandler.cs
+++ Assets/Muse/DesignKits/Examples/DestructionWithPooling/DestructionHandler.cs
@@ -1,47 +1,49 @@
 using System;
 using UnityEngine;
-
-public class DestructionHandler : MonoBehaviour
+namespace Muse
 {
-    [Serializable]
-    public class CollisionHandlerGroup
+    public class DestructionHandler : MonoBehaviour
     {
-        public CollisionHandler[] collisionHandlers;
-    }
-
-    public CollisionHandlerGroup[] collisionHandlerGroups;
-    public ObjectPooler objectPooler;
-
-    private void Awake()
-    {
-        for (int i = 0; i < collisionHandlerGroups.Length; i++)
+        [Serializable]
+        public class CollisionHandlerGroup
         {
-            var group = collisionHandlerGroups[i];
-            var handlers = group.collisionHandlers;
-            for (int j = 0; j < handlers.Length; j++)
+            public CollisionHandler[] collisionHandlers;
+        }
+
+        public CollisionHandlerGroup[] collisionHandlerGroups;
+        public ObjectPooler objectPooler;
+
+        private void Awake()
+        {
+            for (int i = 0; i < collisionHandlerGroups.Length; i++)
             {
-                var handler = handlers[j];
-                var index = i;
-                Action<Collision> callback = (c) =>
+                var group = collisionHandlerGroups[i];
+                var handlers = group.collisionHandlers;
+                for (int j = 0; j < handlers.Length; j++)
                 {
-                    var pos = handler.transform.position;
-                    var rot = handler.transform.rotation;
-                    handler.gameObject.SetActive(false);
-                    var g = objectPooler.DrawFromPool(index);
-
-                    var rbs = g.GetComponentsInChildren<Rigidbody>();
-                    for (int k = 0; k < rbs.Length; k++)
+                    var handler = handlers[j];
+                    var index = i;
+                    Action<Collision> callback = (c) =>
                     {
-                        var rb = rbs[k];
-                        rb.transform.localPosition = Vector3.zero;
-                        rb.transform.localRotation = Quaternion.identity;
-                    }
+                        var pos = handler.transform.position;
+                        var rot = handler.transform.rotation;
+                        handler.gameObject.SetActive(false);
+                        var g = objectPooler.DrawFromPool(index);
 
-                    g.transform.position = pos;
-                    g.transform.rotation = rot;
-                };
-                // Debug.Log(i);
-                handler.Init(callback);
+                        var rbs = g.GetComponentsInChildren<Rigidbody>();
+                        for (int k = 0; k < rbs.Length; k++)
+                        {
+                            var rb = rbs[k];
+                            rb.transform.localPosition = Vector3.zero;
+                            rb.transform.localRotation = Quaternion.identity;
+                        }
+
+                        g.transform.position = pos;
+                        g.transform.rotation = rot;
+                    };
+                    // Debug.Log(i);
+                    handler.Init(callback);
+                }
             }
         }
     }
--- Assets/Muse/DesignKits/Examples/ObjectPooler/ExampleObjectPooler.cs
+++ Assets/Muse/DesignKits/Examples/ObjectPooler/ExampleObjectPooler.cs
@@ -1,29 +1,32 @@
 // using Muse;
 using UnityEngine;
 
-public class ExampleObjectPooler : MonoBehaviour
+namespace Muse
 {
-    public ObjectPooler objectPooler;
-
-    void Update()
+    public class ExampleObjectPooler : MonoBehaviour
     {
-        if (Input.GetMouseButtonDown(0))
-            Spawn(0);
-        else if (Input.GetMouseButtonDown(1))
-            Spawn(1);
-        else if (Input.GetKeyDown(KeyCode.Space))
-            Spawn(2);
-        else if (Input.GetKeyDown(KeyCode.C))
-            objectPooler.CleanupAllPools();
-        else if (Input.GetKeyDown(KeyCode.A))
-            objectPooler.CleanupPool(0);
-    }
+        public ObjectPooler objectPooler;
 
-    void Spawn(int index)
-    {
-        var g = objectPooler.DrawFromPool(index);
-        g.transform.position = Vector3.zero;
-        g.transform.rotation = Quaternion.identity;
-        g.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+                Spawn(0);
+            else if (Input.GetMouseButtonDown(1))
+                Spawn(1);
+            else if (Input.GetKeyDown(KeyCode.Space))
+                Spawn(2);
+            else if (Input.GetKeyDown(KeyCode.C))
+                objectPooler.CleanupAllPools();
+            else if (Input.GetKeyDown(KeyCode.A))
+                objectPooler.CleanupPool(0);
+        }
+
+        void Spawn(int index)
+        {
+            var g = objectPooler.DrawFromPool(index);
+            g.transform.position = Vector3.zero;
+            g.transform.rotation = Quaternion.identity;
+            g.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
     }
 }
